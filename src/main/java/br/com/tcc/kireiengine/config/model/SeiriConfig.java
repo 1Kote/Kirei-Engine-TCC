a8f9b4dc25525a7d01f5f12a453ac84291d01ed4
1Kote <lioirq1@gmail.com>
2025-08-15 12:00:20 -0300

    feat: adição das configurações de Seiri e Seiso

--- src/main/java/br/com/tcc/kireiengine/config/model/SeiriConfig.java
+++ src/main/java/br/com/tcc/kireiengine/config/model/SeiriConfig.java
@@ -1,9 +1,22 @@
 package br.com.tcc.kireiengine.config.model;
 
+import java.util.concurrent.TimeUnit;
+
 public class SeiriConfig
 {
+        //Atributos de classe
+        private boolean enabled;
+        private long initialDelay;
+        private long period;
+        private TimeUnit timeUnit;
 
-
-
-
+        // Getters
+        public boolean isEnabled() { return enabled; }
+        public long getInitialDelay() { return initialDelay; }
+        public long getPeriod() { return period; }
+        public TimeUnit getTimeUnit() { return timeUnit; }
 }
+
+
+
+
--- src/main/java/br/com/tcc/kireiengine/config/model/SeisoConfig.java
+++ src/main/java/br/com/tcc/kireiengine/config/model/SeisoConfig.java
@@ -1,8 +1,19 @@
 package br.com.tcc.kireiengine.config.model;
 
+import java.util.concurrent.TimeUnit;
+
 public class SeisoConfig
 {
+    //Atributos de classe
+    private boolean enabled;
+    private long initialDelay;
+    private long period;
+    private TimeUnit timeUnit;
 
 
-
+    // Getters
+    public boolean isEnabled() { return enabled; }
+    public long getInitialDelay() { return initialDelay; }
+    public long getPeriod() { return period; }
+    public TimeUnit getTimeUnit() { return timeUnit; }
 }
