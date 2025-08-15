846114ac77060d695e9d46d9ac8eac5c82fa618e
1Kote <lioirq1@gmail.com>
2025-08-15 08:19:29 -0300

    feat: criação dos atributos privados e encapsulamento.

--- src/main/java/br/com/tcc/kireiengine/config/model/SeitonRule.java
+++ src/main/java/br/com/tcc/kireiengine/config/model/SeitonRule.java
@@ -1,8 +1,41 @@
 package br.com.tcc.kireiengine.config.model;
 
+import java.util.List;
+
 public class SeitonRule
 {
+    private String name;
+    private List<String> extensions;
+    private String destination;
 
+    //Getters
+    public String getName()
+    {
+         return name;
+    }
+    public List<String> getExtensions()
+    {
+        return extensions;
+    }
+    public String getDestination()
+    {
+        return destination;
+    }
 
+    //Setters
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setExtensions(List<String> extensions)
+    {
+        this.extensions = extensions;
+    }
+
+    public void setDestination(String destination)
+    {
+        this.destination = destination;
+    }
 
 }
