d409b8e428e27c8b32020ebae7e0428d0e909606
1Kote <lioirq1@gmail.com>
2025-08-15 08:35:27 -0300

    feat: Criação da classe Configuration assim como seus atributos privados e encapsulamento.

---
+++ src/main/java/br/com/tcc/kireiengine/config/model/Configuration.java
@@ -0,0 +1,55 @@
+package br.com.tcc.kireiengine.config.model;
+
+import java.util.List;
+
+public class Configuration
+{
+    private List<String> monitorFolders;
+    private List<SeitonRule> seitonRules;
+    private SeiriConfig seiriConfig;
+    private SeisoConfig seisoConfig;
+
+    //Getters
+    public List<String> getMonitorFolders()
+    {
+        return monitorFolders;
+    }
+
+    public List<SeitonRule> getSeitonRules()
+    {
+        return seitonRules;
+    }
+
+    public SeiriConfig getSeiriConfig()
+    {
+        return seiriConfig;
+    }
+
+    public SeisoConfig getSeisoConfig()
+    {
+        return seisoConfig;
+    }
+
+    //Setters
+    public void setMonitorFolders(List<String> monitorFolders)
+    {
+        this.monitorFolders = monitorFolders;
+    }
+
+    public void setSeitonRules(List<SeitonRule> seitonRules)
+    {
+        this.seitonRules = seitonRules;
+    }
+
+    public void setSeiriConfig(SeiriConfig seiriConfig)
+    {
+        this.seiriConfig = seiriConfig;
+    }
+
+    public void setSeisoConfig(SeisoConfig seisoConfig)
+    {
+        this.seisoConfig = seisoConfig;
+    }
+
+
+}
