10dba74081cc71bc5902c9801fb13fdbc29c0842
1Kote <lioirq1@gmail.com>
2025-08-18 09:26:34 -0300

    feat: Classe de configuração de regras de Seiri

---
+++ src/main/java/br/com/tcc/kireiengine/config/model/SeiriRuleConfig.java
@@ -0,0 +1,12 @@
+package br.com.tcc.kireiengine.config.model;
+
+public class SeiriRuleConfig
+{
+    private MoveFilesRule moveFilesNotAccessedForDays;
+
+    //Getters
+    public MoveFilesRule getMoveFilesNotAccessedForDays()
+    {
+        return moveFilesNotAccessedForDays;
+    }
+}
