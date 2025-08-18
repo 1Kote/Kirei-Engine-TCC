118e3405d387818343ad350fff6260b1c313f8d0
1Kote <lioirq1@gmail.com>
2025-08-18 09:25:55 -0300

    feat: adicionado o atributo rules do tipo SeiriRulesConfig e seu encapsulamento.

--- src/main/java/br/com/tcc/kireiengine/config/model/SeiriConfig.java
+++ src/main/java/br/com/tcc/kireiengine/config/model/SeiriConfig.java
@@ -9,12 +9,14 @@ public class SeiriConfig
         private long initialDelay;
         private long period;
         private TimeUnit timeUnit;
+        private SeiriRuleConfig rules;
 
         // Getters
         public boolean isEnabled() { return enabled; }
         public long getInitialDelay() { return initialDelay; }
         public long getPeriod() { return period; }
         public TimeUnit getTimeUnit() { return timeUnit; }
+        public SeiriRuleConfig getRules() { return rules; }
 }
 
 
