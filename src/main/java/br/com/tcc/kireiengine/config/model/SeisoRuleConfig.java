ce64dd228360ec30162151050d7c4ce1c219fec7
1Kote <lioirq1@gmail.com>
2025-08-19 19:02:20 -0300

    feat: Criação da classe SeisoRuleConfig

---
+++ src/main/java/br/com/tcc/kireiengine/config/model/SeisoRuleConfig.java
@@ -0,0 +1,11 @@
+package br.com.tcc.kireiengine.config.model;
+
+public class SeisoRuleConfig
+{
+    private CleanTemporaryFoldersRule cleanTemporaryFolders;
+
+    public CleanTemporaryFoldersRule getCleanTemporaryFolders()
+    {
+        return cleanTemporaryFolders;
+    }
+}
